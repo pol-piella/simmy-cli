e4941c409b3197469bcfd4a7d5b4e630742d1d23
Pol Piella Abadia <hi@polpiella.dev>
2021-11-22 19:43:12 +0000

    Adds langloc command to allow for easy config change

--- commands/index.js
+++ commands/index.js
@@ -4,4 +4,5 @@ module.exports = [
     require("./boot"),
     require("./pic"),
     require("./rec"),
+    require("./locale"),
 ]
---
+++ commands/locale/index.js
@@ -0,0 +1,27 @@
+// @ts-check
+
+const { Command } = require("commander")
+const selectedDevice = require("../booted-devices")
+const shell = require("../../services/shell")
+
+module.exports = new Command()
+    .command("langloc")
+    .description("Change the locale of a given (booted) simulator")
+    .argument("language", "The language you would want to change the value to")
+    .argument("locale", "The locale you would want to change the value to")
+    .action(async (language, locale) => {
+        const { udid } = await selectedDevice()
+
+        console.log(`🌍 Changing locale to ${language}-${locale}`)
+        const path = `~/Library/Developer/CoreSimulator/Devices/${udid}/data/Library/Preferences/.GlobalPreferences.plist`
+
+        await shell(`plutil -replace AppleLocale -string ${locale} ${path}`)
+        await shell(
+            `plutil -replace AppleLanguages -json "[ \\"${language}\\" ]" ${path}`
+        )
+
+        // TODO: This reboot can be moved into its own command
+        console.log(`🔄  Rebooting simulator...`)
+        await shell(`xcrun simctl shutdown ${udid}`)
+        await shell(`xcrun simctl boot ${udid}`)
+    })
